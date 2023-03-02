89d1f76de205b3cb0435f4e81027f9521336b748
Tom Schneider <tom.schneider@willowtreeapps.com>
2023-03-01 22:13:18 -0500

    WIP thing

---
+++ src/components/BoardPiece.vue
@@ -0,0 +1,38 @@
+<script lang="ts">
+    interface InputEvent extends Event {
+        data: string;
+    }
+    export default {
+        data() {
+            return {
+                value: 0
+            }
+        },
+        methods: {
+            inputChange(payload: InputEvent) {
+                console.log(payload)
+                const oldValue = this.value
+                const number = parseInt(payload.data ?? '')
+                if (!isNaN(number)) {
+                    this.value = number
+                } else {
+                    this.value = oldValue
+                    return 
+                }
+            }
+        }
+        
+    }
+</script>
+
+<template>
+    <input
+        @input="inputChange"
+        v-model="value"/>
+</template>
+
+<style scoped>
+    button {
+        font-weight: bold;
+    }
+</style>
---
+++ src/views/BoardView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+    import BoardPiece from '../components/BoardPiece.vue'
+</script>
+
+<template>
+  <main>
+    <BoardPiece/>
+  </main>
+</template>
